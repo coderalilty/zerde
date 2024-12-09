512bbe2553435c1dfb5cffe4f92c02daa9ceb32f
Elzat-developer <fergenson.2020@gmail.com>
2024-12-09 18:03:41 +0500

    first commit

---
+++ src/test/java/kidd/house/zerde/ZerdeApplicationTests.java
@@ -0,0 +1,13 @@
+package kidd.house.zerde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerdeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
