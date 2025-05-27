a8a54ed1af517f1ca7465425e07f86f81ad00146
Tilegen <tilegenjuban@gmail.com>
2025-05-28 01:48:58 +0500

    automated deploy main branch

--- src/test/java/kidd/house/zerde/ZerdeApplicationTests.java
+++ src/test/java/kidd/house/zerde/ZerdeApplicationTests.java
@@ -2,8 +2,10 @@ package kidd.house.zerde;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ZerdeApplicationTests {
 
     @Test
