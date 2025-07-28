f2cb61840b9c2d6edf041452afa0f88c40126cbc
Tilegen <tilegenjuban@gmail.com>
2025-07-28 22:38:05 +0500

    try to fix

--- init.sql
+++ init.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS `zerde-kidd-house` collate utf8mb4_unicode_ci;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+CREATE USER 'root'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
