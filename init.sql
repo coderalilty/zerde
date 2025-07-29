45b92c0e0f60f807d8795c75b7f0572d88b18cff
Elzat-developer <fergenson.2020@gmail.com>
2025-07-29 16:05:06 +0500

    касяки может сейчас запустится

--- init.sql
+++ init.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE IF NOT EXISTS `zerde-kidd-house` collate utf8mb4_unicode_ci;
-CREATE USER 'root'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
