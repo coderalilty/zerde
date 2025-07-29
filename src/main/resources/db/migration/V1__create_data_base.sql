15ece0f79849fb4affa675ff40d3bb703ff998a7
Elzat-developer <fergenson.2020@gmail.com>
2025-07-29 15:56:02 +0500

    касяки может сейчас запустится

--- src/main/resources/db/migration/V1__create_data_base.sql
+++ src/main/resources/db/migration/V1__create_data_base.sql
@@ -116,7 +116,8 @@ CREATE TABLE IF NOT EXISTS `users` (
                                        `name` varchar(255) DEFAULT NULL,
                                        `surname` varchar(255) DEFAULT NULL,
                                        `lastname` varchar(255) DEFAULT NULL,
-                                       `password` varchar(255) DEFAULT NULL
+                                       `password` varchar(255) DEFAULT NULL,
+                                       `password_temporary` BOOLEAN DEFAULT FALSE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `groups` (
--- src/main/resources/db/migration/V5__add_column_password_temporary.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE users
-    ADD COLUMN password_temporary BOOLEAN DEFAULT FALSE;
-
-UPDATE Users
-SET password_temporary = false
-WHERE id = 1;
-
